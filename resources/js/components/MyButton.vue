026fa68e298b55ff73ac6c54933a9edd0564804b
mrizkir <mr.romdoni@gmail.com>
2019-10-19 05:33:43 +0700

    test vue 1

---
+++ resources/js/components/MyButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <button :type="type" class="my-button" v-text="test"></button>
+    </div>
+</template>
+
+<script>
+    export default 
+    {
+        mounted() 
+        {
+            console.log('Component mounted.')
+        },
+
+        data : function ()
+        {
+            return {
+                test:'lier'
+            }
+        },
+
+        props : [
+            'text',
+            'type'
+        ]
+    }
+</script>
+<style>
+    .my-button {
+        background-color: #333;
+        color:#fff;
+        padding: 10px 20px;
+        font-weight: bold;
+    }
+
+</style>
